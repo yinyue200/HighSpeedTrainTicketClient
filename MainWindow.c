a88b1617aa09cb88ce70ca58ec32d2c33c92f02a
yinyue200 <yinyue200@outlook.com>
2021-07-08 01:29:22 +0800

    关于信息中增加版权相关

--- MainWindow.c
+++ MainWindow.c
@@ -1115,7 +1115,10 @@ LRESULT CALLBACK MainWindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lPar
             ShellExecute(NULL, L"Open", L"https://github.com/yinyue200/SimpleStoreErp", NULL, NULL, SW_SHOWNORMAL);
             break;
         case ID_MENU_ABOUT:
-            MessageBox(hwnd, L"版本 0.0.1\r\nA GUI program\r\nold fashion design", L"关于", 0);
+            MessageBox(hwnd, L"版本 0.0.1\r\nA GUI program\r\nold fashion design\n\nCopyright (C) 2021 殷越\n"
+                L"This program comes with ABSOLUTELY NO WARRANTY.\n"
+                L"This is free software, and you are welcome to redistribute it under certain conditions.\n"
+                L"See https://github.com/yinyue200/SimpleStoreErp for more details.", L"关于", 0);
             break;
         case ID_MENU_SAVE:
             yinyue200_ProductRecordSaveToFile(yinyue200_GetConfigFilePath(), &yinyue200_ProductList);
