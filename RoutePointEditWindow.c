2df8c1280fab4322c1f0890138de23dc664ea7d3
yinyue200 <yinyue200@outlook.com>
2022-01-04 15:13:46 +0800

    修正错误的文件名

--- RoutePointEditWindow.c
+++ RoutePointEditWindow.c
@@ -13,7 +13,7 @@
 //
 //	You should have received a copy of the GNU General Public License
 //	along with this program. If not, see <https://www.gnu.org/licenses/>.
-#include "CreateRoutePointEditWindow.h"
+#include "RoutePointEditWindow.h"
 #include "common.h"
 #include "ControlsCommonOperation.h"
 #include "ProductRecord.h"
