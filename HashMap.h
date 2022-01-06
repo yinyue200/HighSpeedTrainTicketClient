1db723d09cb7d66f404650b3820698731135aee6
yinyue200 <yinyue200@outlook.com>
2022-01-06 16:43:04 +0800

    修复函数签名不一致问题

--- HashMap.h
+++ HashMap.h
@@ -51,7 +51,7 @@ void* HashMap_GetFromKey(HASHMAP* map, void* key);
 void* HashMap_RemoveByKey(HASHMAP* map, void* key);
 void HashMap_CheckAndResize(HASHMAP* map);
 void** HashMap_GetPointerByKey(HASHMAP* map, void* key, bool allowadd);
-HASHMAPNODE* HashMap_GetPointersByKey(HASHMAP* map, void* key, HASHMAPNODE* lastnode);
+HASHMAPNODE* HashMap_GetPointersByKey(HASHMAP* map, void* key, HASHMAPNODE* lastnode, size_t* maxposscount);
 void HashMap_Rehash(HASHMAP* map);
 void HashMap_Add(HASHMAP* map, void* item);
 inline void HashSet_NoFree(void* obj){}
