592cc8336d7b4ebd23019daf63a50d5f9e7192da
mercy0916 <93861426+mercy0916@users.noreply.github.com>
2022-01-10 10:16:09 +0800

    补充了common文件的注释

--- common.h
+++ common.h
@@ -56,6 +56,11 @@ wchar_t* CreateWstrForWindowText(HWND hwnd);
 //将传入的字符串复制一次
 //str：要复制的字符串
 wchar_t* CreateWstrFromWstr(wchar_t *hwnd);
+/// <summary>
+/// 一种原创的转义算法
+/// </summary>
+/// <param name="str"></param>
+/// <returns></returns>
 wchar_t* Yinyue200_TsvEncode(wchar_t* str);
 wchar_t* Yinyue200_TsvDecode(wchar_t* str);
 //无法恢复的错误，直接退出程序
@@ -81,12 +86,23 @@ uint64_t Yinyue200_ConvertToUINT64FromTotalSecond(double time);
 //将vector中的内容序列化到字符串
 //func: 将 vector 的每一项序列化的函数
 PWSTR Yinyue200_ConvertVectorToString(vector* vec, PWSTR(*func)(void* ptr));
+/// <summary>
+/// 反序列化一个列表
+/// </summary>
+/// <param name="str"></param>
+/// <param name="func"></param>
+/// <returns></returns>
 vector Yinyue200_ConvertStringToVector(PWSTR str, void* (*func)(PWSTR str));
 //输入的是本地时间
 FILETIME ConvertDateToUTCFILETIME(int year, int month, int day);
 //输入的是本地时间
 FILETIME ConvertDateToLocalFILETIME(int year, int month, int day);
 uint64_t ConvertTimeToUINT64(UINT hour, UINT minute, UINT second);
+/// <summary>
+/// 获取一个日期时间的日期部分
+/// </summary>
+/// <param name="time">日期时间</param>
+/// <returns></returns>
 uint64_t GetDatePartUINT64OFUINT64(uint64_t time);
 /// <summary>
 /// 将国际时间转换为本地时间并获取其日期部分
@@ -94,13 +110,44 @@ uint64_t GetDatePartUINT64OFUINT64(uint64_t time);
 /// <param name="time">国际时间</param>
 /// <returns></returns>
 uint64_t GetLocalDatePartUINT64OFUINT64(uint64_t time);
+/// <summary>
+/// 将一个UTC日期时间转换为本地时间，并获取其时间部分
+/// </summary>
+/// <param name="time"></param>
+/// <returns></returns>
 uint64_t GetLocalTimePartUINT64OFUINT64(uint64_t time);
+/// <summary>
+/// 获取一个日期时间的时间部分
+/// </summary>
+/// <param name="time">日期时间</param>
+/// <returns></returns>
 uint64_t GetTimePartUINT64OFUINT64(uint64_t time);
+/// <summary>
+/// 将字符串分割为列表
+/// </summary>
+/// <param name="str">要分割的字符串</param>
+/// <param name="spl">分割字符串的子串</param>
+/// <returns></returns>
 vector SplitStringToVectorOfString(PWSTR str, PWSTR spl);
+/// <summary>
+/// 析构一个字符串列表
+/// </summary>
+/// <param name="vec"></param>
 void FreeVectorOfString(vector* vec);
 //写入宽字符串到文件
 bool WritePWSTR(PCWSTR str, HANDLE hFile);
+/// <summary>
+/// 计算宽字符串的哈希值
+/// </summary>
+/// <param name="str"></param>
+/// <returns></returns>
 uint64_t xxHashPWSTR(PWSTR str);
+/// <summary>
+/// 比较两个宽字符串
+/// </summary>
+/// <param name="left"></param>
+/// <param name="right"></param>
+/// <returns></returns>
 bool ComparePWSTR(PCWSTR left, PCWSTR right);
 inline void Yinyue200_FreeAndClear(void** str)
 {
@@ -164,7 +211,24 @@ inline PWSTR Yinyue200_GetPWSTRWithoutNull(PWSTR str)
 		return L"";
 	return str;
 }
-
+/// <summary>
+/// 将本地日期时间转换为UTC日期时间
+/// </summary>
+/// <param name="time"></param>
+/// <returns></returns>
 uint64_t Yinyue200_ConvertLocalUint64ToUtcUint64(uint64_t time);
+/// <summary>
+/// 判断字符串中是否包含某个字符
+/// </summary>
+/// <param name="str"></param>
+/// <param name="one"></param>
+/// <returns></returns>
 bool PWSTRContainChar(PWSTR str, WCHAR one);
+/// <summary>
+/// 替换一个字符串中的所有某个字符
+/// </summary>
+/// <param name="str">要处理的字符串</param>
+/// <param name="old">被替换的字符</param>
+/// <param name="newone">新字符</param>
+/// <returns></returns>
 PWSTR Yinyue200_ReplacePWSTR(PWSTR str, WCHAR old, WCHAR newone);
